9d567a99f22acb56cb98ff64f38b1e7013402f48
TomScott <contact@tascott.co.uk>
2023-03-05 22:33:55 +0000

    Add an example CV from the web to test file downloads

--- src/pages/ProjectPage.js
+++ src/pages/ProjectPage.js
@@ -5,23 +5,23 @@ import projectslist from "../projectslist.json";
 function Project() {
   const { slug } = useParams();
   const project = projectslist[slug];
-  const { title, description, image, repo, url  } = project;
+  const { title, description, image, repo, url } = project;
 
   return (
     <div className="project project-sole">
-    <img src={image} alt="project"></img>
-    <div className="project-text">
-      <h3>{title}</h3>
-      <p>{description}</p>
-      <a className="bio-link" href={repo} target="_blank" rel="noreferrer">
-        Github Repository
-      </a>
-      <a className="bio-link" href={url} target="_blank" rel="noreferrer">
-        Live Application
+      <img src={image} alt="project"></img>
+      <div className="project-text">
+        <h3>{title}</h3>
+        <p>{description}</p>
+        <a className="bio-link" href={repo} target="_blank" rel="noreferrer">
+          Github Repository
         </a>
-      <Link to="/projects">Back to Projects</Link>
+        <a className="bio-link" href={url} target="_blank" rel="noreferrer">
+          Live Application
+        </a>
+        <Link to="/projects">Back to Projects</Link>
+      </div>
     </div>
-  </div>
   );
 }
 
